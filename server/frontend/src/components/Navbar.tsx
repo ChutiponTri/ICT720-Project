cd26b016d9c62926ef485c899506f97a547cf117
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2025-03-24 14:36:04 +0700

    Update Navbar.tsx uncomment db module

--- server/frontend/src/components/Navbar.tsx
+++ server/frontend/src/components/Navbar.tsx
@@ -5,8 +5,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import { syncUser } from "@/actions/user.action";
 
 async function Navbar() {
-  // const user = await currentUser();
-  // if (user) await syncUser(); // POST
+  const user = await currentUser();
+  if (user) await syncUser(); // POST
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
