1f52d377e99c7f5619c0587430c978b3cfd0144b
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2025-03-28 19:19:03 +0700

    Adjust Code Template

--- Demo T-SimCam BLE/test/get_post.cpp
+++ Demo T-SimCam BLE/test/get_post.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
@@ -6,34 +7,58 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"  // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-#define ssid "MARK"
+#define ssid "EZEKIEL"
 #define pass "late1978"
 #define mqtt_broker "broker.emqx.io"
 #define mqtt_port 1883
 #define post_topic "ton/server/post"
 #define get_topic "ton/server/get"
+#define ble_dev "M5"
 
+void ble_scan(void);
+void connection(void);
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify);
+void setup_wifi(void);
+void setup_mqtt(void);
+void reconnect_mqtt(void);
+void callback_mqtt(char* topic, byte* payload, unsigned int length);
+void send(String message);
+void falling_check(float az);
+
+// BLE Service
 BLEUUID serviceUUID(SERVICE_UUID);
 BLEUUID serviceRX(CHARACTERISTIC_UUID_RX);
 BLEUUID serviceTX(CHARACTERISTIC_UUID_TX);
 
+// BLE Callback
 int scanTime = 5;  //In seconds
 BLEScan *pBLEScan;
 BLEAdvertisedDevice *device = NULL;
 BLERemoteCharacteristic *pRemoteCharacteristic;
+BLEClient *pClient;
+BLERemoteService *pRemoteService;
 bool tryConnecting = false;
 bool connected = false;
 char buf[256];
+String clientName;
+std::string dev_name;
 
+// WiFi Variables
 WiFiClient espclient;
 PubSubClient client(espclient);
 
+// Request Variables
+const char* host = "notify-api.line.me";
+const char* token = "b15ajPr1mhglSe58uxBzngKqgvVxx1DAvaYE5mIlQtS";
+WiFiClientSecure http_client;
+String line, query, request;
+
+// IMU Variables
 typedef struct {
   float ax;
   float ay;
@@ -43,25 +68,19 @@ typedef struct {
   float gz;
 } IMUData;
 
-uint16_t count = 0;
+uint8_t count = 0;
 IMUData data[5];
 bool is_falling = false;
 float falling_count = 0;
-
-void ble_scan(void);
-void connection(void);
-void setup_wifi(void);
-void setup_mqtt(void);
-void reconnect_mqtt(void);
-void callback_mqtt(char* topic, byte* payload, unsigned int length);
-void send(String message);
-void falling_check(float az);
+JsonDocument doc;
+DeserializationError error;
+char mqtt_buf[256];
 
 // Advertise Callback
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
-    std::string dev_name = advertisedDevice.getName().c_str();
-    if ((sizeof(dev_name) > 0) && (dev_name.find("XIAO YOYO") != std::string::npos)){
+    dev_name = advertisedDevice.getName().c_str();
+    if ((sizeof(dev_name) > 0) && (dev_name.find(ble_dev) != std::string::npos)){
       device = new BLEAdvertisedDevice(advertisedDevice);
       Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
       advertisedDevice.getScan()->stop();
@@ -82,79 +101,6 @@ class MyClientCallback : public BLEClientCallbacks {
   }
 };
 
-// Notify Callback
-static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
-  client.publish(post_topic, pData, length);
-  Serial.write(pData, length);
-  Serial.println();
-
-  /*  // Create a StaticJsonDocument with enough capacity
-  JsonDocument doc;
-
-  // Parse the JSON data
-  DeserializationError error = deserializeJson(doc, pData);
-  if (error) {
-      Serial.print("deserializeJson() failed: ");
-      Serial.println(error.f_str());
-      return;
-  }
-
-  // Retrieve values from the JSON object
-  float ax = doc["ax"]; 
-  float ay = doc["ay"]; 
-  float az = doc["az"]; 
-  float gx = doc["gx"]; 
-  float gy = doc["gy"]; 
-  float gz = doc["gz"]; 
-
-  // Print the values
-  Serial.print("ax: ");
-  Serial.print(ax);
-  Serial.print(",ay: ");
-  Serial.print(ay);
-  Serial.print(",az: ");
-  Serial.print(az);
-  Serial.print(",gx: ");
-  Serial.print(gx);
-  Serial.print(",gy: ");
-  Serial.print(gy);
-  Serial.print(",gz: ");
-  Serial.print(gz);
-  Serial.println();
-
-  // // Access data
-  data[count].ax = ax;
-  data[count].ay = ay;
-  data[count].az = az;
-  data[count].gx = gx;
-  data[count].gy = gy;
-  data[count].gz = gz;
-
-  falling_check(az);
-
-  count ++;
-
-  if (client.connected() && (count == 5)){
-    char mqtt_buf[256];
-    snprintf(mqtt_buf, sizeof(mqtt_buf),
-      "{\"ax1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
-      "\"ay1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
-      "\"az1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
-      "\"gx1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
-      "\"gy1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
-      "\"gz1\":[%.2f,%.2f,%.2f,%.2f,%.2f]}",
-      data[0].ax, data[1].ax, data[2].ax, data[3].ax, data[4].ax,
-      data[0].ay, data[1].ay, data[2].ay, data[3].ay, data[4].ay,
-      data[0].az, data[1].az, data[2].az, data[3].az, data[4].az,
-      data[0].gx, data[1].gx, data[2].gx, data[3].gx, data[4].gx,
-      data[0].gy, data[1].gy, data[2].gy, data[3].gy, data[4].gy,
-      data[0].gz, data[1].gz, data[2].gz, data[3].gz, data[4].gz
-    );
-    client.publish(post_topic, mqtt_buf);
-    count = 0;
-  }*/
-}
-
 // Function to Initialize Device
 void setup(void) {
   Serial.begin(115200);
@@ -166,7 +112,7 @@ void setup(void) {
 // Function to Loop Device
 void loop(void) {
   if (WiFi.status() != WL_CONNECTED){
-    setup_wifi();
+    WiFi.reconnect();
   }
 
   if (!client.connected()){
@@ -187,7 +133,6 @@ void loop(void) {
   }
 
   delay(100);
-
 }
 
 // Function to Scan BLE device
@@ -211,38 +156,87 @@ void ble_scan(void){
 
 // Function to Start BLE Connection
 void connection(){
-    BLEClient *pClient = BLEDevice::createClient();
-    pClient->setClientCallbacks(new MyClientCallback());
-    pClient->connect(device);
-    pClient->setMTU(517);
+  pClient = BLEDevice::createClient();
+  pClient->setClientCallbacks(new MyClientCallback());
+  pClient->connect(device);
+  pClient->setMTU(517);
 
-    // Obtain a reference to the service we are after in the remote BLE server.
-    BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-      Serial.print("Failed to find our service UUID: ");
-      Serial.println(serviceUUID.toString().c_str());
-      pClient->disconnect();
+  // Obtain a reference to the service we are after in the remote BLE server.
+  pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return;
+  }
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(serviceTX);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(serviceTX.toString().c_str());
+    pClient->disconnect();
+    return;
+  }
+
+  // Start Notify
+  if (pRemoteCharacteristic->canNotify()) {
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+  } else {
+    Serial.println("Cannot Notify");
+  }
+
+  connected = true;
+  tryConnecting = false;
+}
+
+// Notify Callback
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+  client.publish(post_topic, pData, length);
+
+  // Parse the JSON data
+  error = deserializeJson(doc, pData);
+  if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.f_str());
       return;
-    }
+  }
+  // Retrieve values from the JSON object
+  data[count].ax = doc["ax"]; 
+  data[count].ay = doc["ay"]; 
+  data[count].az = doc["az"]; 
+  data[count].gx = doc["gx"];
+  data[count].gy = doc["gy"]; 
+  data[count].gz = doc["gz"]; 
 
-    // Obtain a reference to the characteristic in the service of the remote BLE server.
-    pRemoteCharacteristic = pRemoteService->getCharacteristic(serviceTX);
-    if (pRemoteCharacteristic == nullptr) {
-      Serial.print("Failed to find our characteristic UUID: ");
-      Serial.println(serviceTX.toString().c_str());
-      pClient->disconnect();
-      return;
-    }
+  // Print the values
+  Serial.printf("ax: %.2f, ay: %.2f, az: %.2f, gx: %.2f, gy: %.2f, gz: %.2f\n", 
+    data[count].ax, data[count].ay, data[count].az,
+    data[count].gx, data[count].gy, data[count].gz
+  );
 
-    // Start Notify
-    if (pRemoteCharacteristic->canNotify()) {
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
-    } else {
-      Serial.println("Cannot Notify");
-    }
+  falling_check(doc["az"]);
 
-    connected = true;
-    tryConnecting = false;
+  if (client.connected() && (count == 5)){
+    snprintf(mqtt_buf, sizeof(mqtt_buf),
+      "{\"ax1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
+      "\"ay1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
+      "\"az1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
+      "\"gx1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
+      "\"gy1\":[%.2f,%.2f,%.2f,%.2f,%.2f],"
+      "\"gz1\":[%.2f,%.2f,%.2f,%.2f,%.2f]}",
+      data[0].ax, data[1].ax, data[2].ax, data[3].ax, data[4].ax,
+      data[0].ay, data[1].ay, data[2].ay, data[3].ay, data[4].ay,
+      data[0].az, data[1].az, data[2].az, data[3].az, data[4].az,
+      data[0].gx, data[1].gx, data[2].gx, data[3].gx, data[4].gx,
+      data[0].gy, data[1].gy, data[2].gy, data[3].gy, data[4].gy,
+      data[0].gz, data[1].gz, data[2].gz, data[3].gz, data[4].gz
+    );
+    client.publish(post_topic, mqtt_buf);
+  }
+
+  count = (count == 4) ? 0 : count + 1;
+  doc.clear();
 }
 
 // Function to Setup WiFi Connection
@@ -266,7 +260,7 @@ void setup_mqtt(void){
 void reconnect_mqtt(void){
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String clientName = "Alient-" + String(random(0xffff), HEX);
+    clientName = "Alient-" + String(random(0xffff), HEX);
     if (client.connect(clientName.c_str())) {
       Serial.println("connected");
       client.subscribe(get_topic);
@@ -298,10 +292,6 @@ void callback_mqtt(char* topic, byte* payload, unsigned int length){
 
 // Function to Request Line API
 void send(String message) {
-  const char* host = "notify-api.line.me";
-  const char* token = "b15ajPr1mhglSe58uxBzngKqgvVxx1DAvaYE5mIlQtS";
-  // const char* token = "TtK9sTE06I8itQbl75gFdcwjdYertYmIbQEhTr7V0Mg";
-  WiFiClientSecure http_client;
   Serial.println("Try to Notify");
   http_client.setInsecure();
   if (!http_client.connect(host, 443)) {
@@ -309,26 +299,26 @@ void send(String message) {
     return;
   }
   Serial.println("Notify Connected");
-  String query = String("message=") + message;
-  String request = String("") +
-                "POST /api/notify HTTP/1.1" + "\r\n" +
-                "Host: " + String(host) + "\r\n" + 
-                "Authorization: Bearer " + String(token) + "\r\n" +
-                "Content-Length: " + String(query.length()) + "\r\n" +
-                "Content-Type: application/x-www-form-urlencoded" + "\r\n\r\n" + // Blank line between headers and body
-                query + "\r\n";
+  query = String("message=") + message;
+  request = String("") +
+    "POST /api/notify HTTP/1.1" + "\r\n" +
+    "Host: " + String(host) + "\r\n" + 
+    "Authorization: Bearer " + String(token) + "\r\n" +
+    "Content-Length: " + String(query.length()) + "\r\n" +
+    "Content-Type: application/x-www-form-urlencoded" + "\r\n\r\n" + // Blank line between headers and body
+    query + "\r\n";
   http_client.print(request);
 
   // Wait for the response
   while (http_client.connected()) {
-    String line = http_client.readStringUntil('\n');
+    line = http_client.readStringUntil('\n');
     if (line == "\r") {
       break;
     }
   }
 
   // Read response body
-  String line = http_client.readStringUntil('\n');
+  line = http_client.readStringUntil('\n');
 }
 
 // Function to Check Wheelchair Falling Status
--- Demo T-SimCam BLE/test/request.cpp
+++ Demo T-SimCam BLE/test/request.cpp
@@ -2,18 +2,24 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-#define ssid "MARK"
+#define ssid "EZEKIEL"
 #define pass "late1978"
-#define host "172.20.10.3"
+#define host "192.168.137.1"
 #define port 8305
 #define notify "notify-api.line.me"
 #define token "b15ajPr1mhglSe58uxBzngKqgvVxx1DAvaYE5mIlQtS"
 
 void setup_wifi(void);
+void get_request(void);
+void post_request(void);
 
 WiFiClient http_client;
+WiFiClientSecure notify_client;
+
 char buf[3][64] = {"", "hello", "lat"};
 uint8_t count = 0;
+String response, api;
+String message, request, header, body;
 
 // Function to Initialize Device
 void setup(void){
@@ -28,21 +34,35 @@ void loop(void){
     setup_wifi();
   }
 
-  String response;
+  // GET from FastAPI and Send Notify
+  get_request();
+  delay(100);
+  post_request();
 
-  // Request GET from Fastapi
+  count = (count > 2) ? 0 : count + 1;
+
+  delay(2000);
+}
+
+// Function to Setup WiFi
+void setup_wifi(void){
+  Serial.print("Connecting WiFi .");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println();
+  Serial.println("WiFi Connected");
+}
+
+// Function to GET Data from FastAPI
+void get_request(void) {
   if (http_client.connect(host, port)){
-    String api = String("") +
-    "GET /" + String(buf[count]) + " HTTP/1.1" + "\r\n" + 
-    "Host: " + String(host) + "\r\n" +
-    "Connection: close" + "\r\n\r\n";
+    api = String("") +
+      "GET /" + String(buf[count]) + " HTTP/1.1" + "\r\n" + 
+      "Host: " + String(host) + "\r\n" +
+      "Connection: close" + "\r\n\r\n";
     http_client.print(api);
-
-    // Send HTTP GET request
-    // http_client.println("GET /" + String(buf[count]) + " HTTP/1.1");       
-    // http_client.println("Host: " + String(host));
-    // http_client.println("Connection: close");
-    // http_client.println();  // Blank line between headers and body
   
     // Wait for the response
     while (http_client.connected()){
@@ -59,53 +79,35 @@ void loop(void){
 
     http_client.stop();
   }
+}
 
-  delay(100);
-
-  WiFiClientSecure notify_client;
+// Function to Post Data to Notify
+void post_request(void) {
   notify_client.setInsecure();
-  
+
   // Requeset POST to Notify -> port 443 Secure HTTPS connection
   if (notify_client.connect(notify, 443)){
     // Send HTTP Post Request
-    String message = "message=" + response;
-    String request = String("") +
-                  "POST /api/notify HTTP/1.1" + "\r\n" +
-                  "Host: " + String(notify) + "\r\n" +
-                  "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
-                  "Content-Length: " + String(message.length()) + "\r\n" +
-                  "Authorization: Bearer " + String(token) + "\r\n\r\n" + // Blank line between headers and body
-                  message + "\r\n";
+    message = "message=" + response;
+    request = String("") +
+      "POST /api/notify HTTP/1.1" + "\r\n" +
+      "Host: " + String(notify) + "\r\n" +
+      "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+      "Content-Length: " + String(message.length()) + "\r\n" +
+      "Authorization: Bearer " + String(token) + "\r\n\r\n" + // Blank line between headers and body
+      message + "\r\n";
     notify_client.print(request);
 
     // Wait for the response
     while (notify_client.connected()){
-      String header = notify_client.readStringUntil('\n');
+      header = notify_client.readStringUntil('\n');
       if (header == "\r"){
         break;
       }
     }
 
     // Read response body
-    String body = notify_client.readString();
+    body = notify_client.readString();
     notify_client.stop();
   }
-
-  count ++;
-  if (count > 2){
-    count = 0;
-  }
-
-  delay(2000);
-}
-
-// Function to Setup WiFi
-void setup_wifi(void){
-  Serial.print("Connecting WiFi .");
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println();
-  Serial.println("WiFi Connected");
 }
